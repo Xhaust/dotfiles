174647c8396d229ea5dc71c063ebf367973fe0cd
Xhaust <28692487+Xhaust@users.noreply.github.com>
2025-11-05 23:31:51 +0100

    feat(nvim): add opts.lua with basic settings

---
+++ .config/nvim/lua/config/opts.lua
@@ -0,0 +1,14 @@
+vim.opt.shiftwidth = 4
+
+vim.o.number = true
+vim.opt.relativenumber = true
+vim.o.cursorline = true
+
+-- Highlight when yanking
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.hl.on_yank()
+  end,
+})
