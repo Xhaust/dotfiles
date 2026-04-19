a74486cacfbcf5b8e0e509e63670dd62d2925019
Xhaust <28692487+Xhaust@users.noreply.github.com>
2026-04-19 19:53:20 +0200

    feat(nvim): add rainbow highlighting to blink.pairs

--- .config/nvim/lua/plugins/blink-pairs.lua
+++ .config/nvim/lua/plugins/blink-pairs.lua
@@ -7,6 +7,16 @@ return {
 
   --- @module 'blink.pairs'
   --- @type blink.pairs.Config
+
+  init = function()
+    local hl = vim.api.nvim_set_hl
+    hl(0, "BlinkPairsOrange", { fg = "#D19A66" })
+    hl(0, "BlinkPairsPurple", { fg = "#C678DD" })
+    hl(0, "BlinkPairsBlue",   { fg = "#61AFEF" })
+    hl(0, "BlinkPairsUnmatched", { fg = "#FF0000", underline = true })
+    hl(0, "BlinkPairsMatchParen", { bg = "#3E4452", bold = true })
+  end,
+
   opts = {
     mappings = {
       -- you can call require("blink.pairs.mappings").enable()
@@ -24,5 +34,24 @@ return {
         ['<C-S-b>'] = 'motion_reverse',
       },
     },
-  }
+  },
+  highlights = {
+      enabled = true,
+      -- requires require('vim._extui').enable({}), otherwise has no effect
+      cmdline = true,
+      -- set to { 'BlinkPairs' } to disable rainbow highlighting
+      groups = { 'BlinkPairsOrange', 'BlinkPairsPurple', 'BlinkPairsBlue' },
+      unmatched_group = 'BlinkPairsUnmatched',
+
+      -- highlights matching pairs under the cursor
+      matchparen = {
+        enabled = true,
+        -- known issue where typing won't update matchparen highlight, disabled by default
+        cmdline = false,
+        -- also include pairs not on top of the cursor, but surrounding the cursor
+        include_surrounding = false,
+        group = 'BlinkPairsMatchParen',
+        priority = 250,
+      },
+    },
 }
